21790e994065de5fb434f525dadf3a2df4749e9b
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-01-30 00:29:22 +0600

    Product filter with categories

--- script.js
+++ script.js
@@ -1,190 +1,50 @@
 
-let incomeExpense = { 
 
-    income: { 
-
-        "rent": 50, 
-
-        "investment": 50, 
-
-        "shopprofit": 200 
-
- 
-
-    }, 
-
-    expense: { 
-
-        "utility": 25, 
-
-        "electricbill": 50, 
-
-        "waterbill": 100 
-
-    } 
-
-} 
-
- 
-
-console.log(incomeExpense) 
-
- 
-
-//Get keys of Objects 
-
-let allkeys = Object.keys(incomeExpense) 
-
- 
-
-console.log(allkeys) 
-
-let inputkey = "income" 
-
-//Check whether the input key is included in the allkeys array 
-
-console.log(allkeys.includes(inputkey)) 
-
- 
-
-if (allkeys.includes(inputkey)) { 
-
- 
-
-    //get key value pair of this nested object 
-
- 
-
-    const nestedObj = incomeExpense[inputkey] 
-
-    console.log("Nested Object is"); 
-
-    console.log(nestedObj) 
-
-    //Get all keys of nested Object 
-
-    let nestedObjectKey = Object.keys(nestedObj) 
-
-    console.log("Keys of nested object are") 
-
-    console.log(nestedObjectKey) 
-
-    //Loop through the object 
-
-    //View all key value of nested object 
-
- 
-
-    let inputViewAll = "ViewAll"; 
-
-    let inputSortByKey = "sortbyNestedKey" 
-
-    let option = inputSortByKey; 
-
-    let preferredNestedkey = "rent" 
-
-    for (const key in nestedObj) { 
-
-        if (option === inputViewAll) { 
-
-            console.log(`${key}: ${nestedObj[key]}`); 
-
-        } 
-
-        if (option === inputSortByKey && nestedObjectKey.includes(preferredNestedkey)) { 
-
-            if (key === preferredNestedkey) { 
-
-                console.log(`${key}: ${nestedObj[key]}`); 
-
-            } 
-
-        } 
-
-    } 
-
-} 
-
- 
-
-//Get total income 
-
-//Get income object 
-
-const incomeObj = incomeExpense["income"] 
-
-console.log(incomeObj) 
-
-//Get all values of the object 
-
-let incomevalues = Object.values(incomeObj) 
-
-//Check if the part is updated 
-
-//let incomesum = 0; 
-
-// incomevalues.forEach((income) => { 
-
-//     incomesum += income; 
-
-// }); 
-
-let incomesum = incomevalues.reduce((total, income) => total + income, 0); 
-
- 
-
-console.log(incomesum) 
-
-const expenseObj = incomeExpense["expense"] 
-
- 
-
-// //Get all values of the object 
-
-let expensevalues = Object.values(expenseObj) 
-
-// //Check if the part is updated 
-
-// let expensesum = 0; 
-
-// expensevalues.forEach((expense) => expensesum += expense); 
-
-//Reduce is better in place of forEach 
-
-let expensesum = expensevalues.reduce((total, income) => total + income, 0); 
-
-console.log(expensesum); 
-
-let loss = expensesum - incomesum 
-
-let profit = incomesum - expensesum 
-
-//if expense sum is greater than income sum you make a profit 
-
-if (expensesum > incomesum) { 
-
-    console.log(`You are on loss of ${loss}`); 
-
-} 
-
-if (expensesum < incomesum) { 
-
-    console.log(`You are on profit of ${profit}`); 
-
-} 
-
- 
-
- 
-
-//Get total expense 
-
- 
-
-if (!allkeys.includes(inputkey)) { 
-
-    console.log("Key not found") 
-
-} 
-
- 
+let products = [
+    {
+        id:1,
+        name: 'john',
+        price: 1200,
+        quantity:10
+    },
+    {
+        id:2,
+        name: 'Jane',
+        price: 1400,
+        quantity:5
+    },
+     {
+        id:3,
+        name: 'Julie',
+        price: 1600,
+        quantity:6
+    },
+    
+];
+let ids = [];
+products.map(arr=>{
+    ids.push(arr.id);
+});
+
+let productId=3;
+
+let isIncluded=ids.includes(productId)
+const index = products.findIndex(object => {
+  return object.id === productId;
+});
+
+console.log(index);
+if(isIncluded){
+    console.log(productId);
+    let filteredProduct=products.filter(product=>{
+      return  product.id===productId
+    });
+    const result = Object.assign({}, ...filteredProduct);
+    console.log(result.quantity);
+    
+    products[index].quantity++;
+    
+    
+}
+
+ console.log(products)
